e7196e5d94773bd971d725dc9ef2ad671d5eb3eb
MasoodJunejo <mjunejo40@yahoo.com>
2019-02-19 07:31:09 -0800

    masood's commit by mehdi raza lakho

--- app/src/main/java/com/vartista/www/vartista/modules/user/StartService.java
+++ app/src/main/java/com/vartista/www/vartista/modules/user/StartService.java
@@ -1,7 +1,9 @@
 package com.vartista.www.vartista.modules.user;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Message;
@@ -10,18 +12,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bcgdv.asia.lib.ticktock.TickTockView;
+import com.squareup.picasso.Picasso;
 import com.valdesekamdem.library.mdtoast.MDToast;
 import com.vartista.www.vartista.R;
 import com.vartista.www.vartista.beans.NotificationsManager;
 import com.vartista.www.vartista.beans.User;
+import com.vartista.www.vartista.modules.provider.ServiceCancelActivity;
 import com.vartista.www.vartista.restcalls.ApiClient;
 import com.vartista.www.vartista.restcalls.ApiInterface;
 import com.vartista.www.vartista.restcalls.SendNotificationApiInterface;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -36,7 +55,8 @@ import retrofit2.Response;
 public class StartService extends AppCompatActivity {
 
     public static ApiInterface apiInterface;
-    TextView timer;
+    TextView timer,service_tittle;
+    ImageView imgview;
     Button startservice, endservice, reset;
     long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
     Handler handler;
@@ -49,12 +69,15 @@ public class StartService extends AppCompatActivity {
         setContentView(R.layout.activity_start_service);
         apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
         timer = (TextView)findViewById(R.id.tvTimer);
+        service_tittle = (TextView)findViewById(R.id.Service_tittle);
+        imgview = (ImageView)findViewById(R.id.imageView7instartserviceuser);
         startservice = (Button)findViewById(R.id.startbutton);
         endservice = (Button)findViewById(R.id.btPause);
         reset = (Button)findViewById(R.id.btReset);
+        int requestservice_id = 114;
         sendNotificationApiInterface = ApiClient.getApiClient().create(SendNotificationApiInterface.class);
         handler = new Handler() ;
-
+            new StartService.Conncetion(StartService.this,requestservice_id).execute();
         startservice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -160,7 +183,116 @@ public class StartService extends AppCompatActivity {
 
 
 
+    class Conncetion extends AsyncTask<String,String ,String > {
+        private int requestservice_id;
+        private ProgressDialog dialog;
 
+        public Conncetion(Context activity, int requestservice_id) {
+            dialog = new ProgressDialog(activity);
+            this.requestservice_id = requestservice_id;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Retriving data Please Wait..");
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            int requestservice_id = 114;
+
+            String result = "";
+
+            final String BASE_URL = "http://vartista.com/vartista_app/requestdetails.php?requestservice_id="+requestservice_id;
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(BASE_URL));
+                HttpResponse response = client.execute(request);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                StringBuffer stringBuffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    stringBuffer.append(line);
+                    break;
+                }
+                reader.close();
+                result = stringBuffer.toString();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                return new String("Exception is " + e.getMessage());
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            try {
+
+
+                JSONObject jsonResult = new JSONObject(result);
+
+                int success = jsonResult.getInt("success");
+                if (success == 1) {
+                    JSONArray services = jsonResult.getJSONArray("services");
+                    for (int j = 0; j < services.length(); j++) {
+                        JSONObject ser1 = services.getJSONObject(j);
+                        String requestservice_id = ser1.getString("requestservice_id");
+                        String user_customer_id = ser1.getString("user_customer_id");
+                        String service_provider_id = ser1.getString("service_provider_id");
+                        String spname = ser1.getString("Spname");
+                        String username = ser1.getString("Uname");
+                        String service_description = ser1.getString("service_description");
+                        String Spimage = ser1.getString("SpImage");
+                        String customerimage = ser1.getString("CustomerPic");
+                        String location = ser1.getString("location");
+                        String request_status = ser1.getString("request_status");
+                        String date = ser1.getString("date");
+                        String service_title = ser1.getString("service_title");
+                        String price = ser1.getString("price");
+                        String name = ser1.getString("name");
+                        String time = ser1.getString("time");
+
+
+                        Picasso.get().load(Spimage).fit().centerCrop()
+                                .placeholder(R.drawable.profile)
+                                .error(R.drawable.profile)
+                                .into(imgview);
+                        service_tittle.setText("Service : "+service_title);
+
+
+
+
+
+
+
+
+
+
+                    }
+
+
+
+
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(),"no data",Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 
 
 
