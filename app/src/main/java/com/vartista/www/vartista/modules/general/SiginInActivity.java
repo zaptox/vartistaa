582707fa0a2dd1a0202b565ffbde6332211a09a6
Vksoni <vijaykumarxonio@outlook.com>
2019-03-25 00:58:44 +0500

    vk soni

--- app/src/main/java/com/vartista/www/vartista/modules/general/SiginInActivity.java
+++ app/src/main/java/com/vartista/www/vartista/modules/general/SiginInActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.valdesekamdem.library.mdtoast.MDToast;
 import com.vartista.www.vartista.R;
@@ -70,6 +71,8 @@ public class SiginInActivity extends AppCompatActivity {
         try {
             getLocationFromAddress("latifabad no 7");
         } catch (IOException e) {
+//            e.printStackTrace();
+            Toast.makeText(this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
         }
         list=new ArrayList<User>();
         signin = findViewById(R.id.sign_in_button);
@@ -110,15 +113,23 @@ public class SiginInActivity extends AppCompatActivity {
 
                     User usergotten = perfromLogin(email1, password1);
 
+//                    Toast.makeText(SiginInActivity.this, "Yes gotten " + usergotten, Toast.LENGTH_SHORT).show();
 
 
                 } catch (Exception e) {
 
+                    // Toast.makeText(SiginInActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
 
 
             }
         });
+
+        //FOR CHECKING GITHUB WORKING
+
+        // Toast.makeText(this, "Mehdi's Commit yayyyyyyyy", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "THIS IS SAAD COMMIT AGAIN", Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, "THIS IS Xoni COMMIT ", Toast.LENGTH_LONG).show();
 
 
     }
@@ -127,7 +138,9 @@ public class SiginInActivity extends AppCompatActivity {
 
     public User perfromLogin(String email1, String password1) {
 
+//        Toast.makeText(this, "in perform function", Toast.LENGTH_SHORT).show();
         Call<User> call = SiginInActivity.apiInterface.performUserLogin(email1, password1);
+//        Call<User> call = SiginInActivity.apiInterface.performUserLogin();
         setUIToWait(true);
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 
@@ -138,7 +151,7 @@ public class SiginInActivity extends AppCompatActivity {
 
 //
                 if (response.isSuccessful()) {
-//                   // MDToast.makeText(SiginInActivity.this, "In response", MDToast.LENGTH_SHORT).show();
+//                   // Toast.makeText(SiginInActivity.this, "In response", Toast.LENGTH_SHORT).show();
                     setUIToWait(false);
                 }
 //
@@ -170,10 +183,16 @@ public class SiginInActivity extends AppCompatActivity {
 
 
                     userLoggedIn = new User(id, busystatus,name, email, password, image, status, contact, created_at, updated_at,gender,sp_status);
+//                    Toast.makeText(SiginInActivity.this, "Response: " + response.body().getResponse() + "--name:" + name, Toast.LENGTH_SHORT).show();
                     addtosharedpreference(userLoggedIn.getId(),userLoggedIn.getBusystatus(),userLoggedIn.getEmail(),userLoggedIn.getPassword(),
                             userLoggedIn.getName(),userLoggedIn.getGender(),userLoggedIn.getSp_status(),
-                            userLoggedIn.getContact());
+                            userLoggedIn.getContact(),image);
+//                    upload_document(userLoggedIn.getName(),userLoggedIn.getPassword(),userLoggedIn.getContact());
+//                    Toast.makeText(SiginInActivity.this, "The User Id is :- "+userLoggedIn.getId()
+//                            +"\n"+"The Name is "+userLoggedIn.getName()
+//                            +"\n"+"The password is "+userLoggedIn.getPassword(), Toast.LENGTH_SHORT).show();
                     setUIToWait(false);
+//                    Toast.makeText(SiginInActivity.this, ""+userLoggedIn, Toast.LENGTH_SHORT).show();
                     //
                     Intent intent = new Intent(SiginInActivity.this, HomeActivity.class);
                     intent.putExtra("user", userLoggedIn);
@@ -183,16 +202,19 @@ public class SiginInActivity extends AppCompatActivity {
 //
 //
                 } else if (response.body().getResponse().equals("failed")) {
-                    MDToast.makeText(SiginInActivity.this, "Login Failed.. Please try again", MDToast.LENGTH_SHORT, MDToast.TYPE_ERROR).show();
+                    //  Toast.makeText(SiginInActivity.this, "Login Failed.. Please try again", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(SiginInActivity.this, "", Toast.LENGTH_SHORT).show();
                     setUIToWait(false);
 
                 }
 //
                 else {
                     setUIToWait(false);
+                    //  Toast.makeText(SiginInActivity.this, "Response: " + response.body().getResponse(), Toast.LENGTH_SHORT).show();
 
                 }
 
+//                Toast.makeText(SiginInActivity.this, "In response's last line", Toast.LENGTH_SHORT).show();
 
 
             }
@@ -200,7 +222,7 @@ public class SiginInActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<User> call, Throwable t) {
 
-              //  MDToast.makeText(SiginInActivity.this, "Login Failed", MDToast.LENGTH_SHORT).show();
+                //  Toast.makeText(SiginInActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                 setUIToWait(false);
 
             }
@@ -224,29 +246,29 @@ public class SiginInActivity extends AppCompatActivity {
     public String getLocationFromAddress(String strAddress) throws IOException {
 
         Geocoder coder = new Geocoder(this);
-       List<Address>is =coder.getFromLocationName(strAddress,5);
+        List<Address>is =coder.getFromLocationName(strAddress,5);
 
-       Geocoder fwd = new Geocoder(this, Locale.US);
-       String st="latifabad no 7";
+        Geocoder fwd = new Geocoder(this, Locale.US);
+        String st="latifabad no 7";
 
-       List<Address> loc=null;
+        List<Address> loc=null;
 
-       try{
-           loc=fwd.getFromLocationName(st,10);
+        try{
+            loc=fwd.getFromLocationName(st,10);
 
-         //  MDToast.makeText(this, ""+loc, MDToast.LENGTH_SHORT).show();
+            //  Toast.makeText(this, ""+loc, Toast.LENGTH_SHORT).show();
 
             loc.get(0).getLongitude();
-         //  MDToast.makeText(this, "Lat: "+loc.get(0).getLatitude() + "long "+loc.get(0).getLongitude(), MDToast.LENGTH_SHORT).show();
+            //  Toast.makeText(this, "Lat: "+loc.get(0).getLatitude() + "long "+loc.get(0).getLongitude(), Toast.LENGTH_SHORT).show();
 
-       }
-       catch (Exception e){}
+        }
+        catch (Exception e){}
 
 
         return "";
     }
 
-    public void addtosharedpreference(int user_id,int busystatus,String email,String Password,String name,String gender, String sp_status,String contact){
+    public void addtosharedpreference(int user_id,int busystatus,String email,String Password,String name,String gender, String sp_status,String contact,String image){
 
         SharedPreferences sharedPreferencespre =getSharedPreferences("Login", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferencespre.edit();
@@ -258,16 +280,67 @@ public class SiginInActivity extends AppCompatActivity {
         editor.putString("name",name);
         editor.putString("gender",gender);
         editor.putString("sp_status",sp_status);
+        editor.putString("image",image);
+
 
         editor.apply();
         editor.commit();
+        Toast.makeText(this, "Shared PReference are working "+user_id+"\n"+Password, Toast.LENGTH_SHORT).show();
 
 
 
 
     }
 
+//public void upload_document(String Name,String Password,String ContactNo){
+//    setUIToWait(true);
+//    progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+//    Call<Doument_Upload_Nil> call=SiginInActivity.apiInterface.document_upload_nil(Name,Password,ContactNo);
+//    call.enqueue(new Callback<Doument_Upload_Nil>() {
+//        @Override
+//        public void onResponse(Call <Doument_Upload_Nil> call, Response<Doument_Upload_Nil> response) {
+//
+//            if(response.body().getResponse().equals("ok")){
+//                setUIToWait(false);
+//
+//                Toast.makeText(SiginInActivity.this,"Updated Successfully..",Toast.LENGTH_SHORT).show();
+//
+//            }else if(response.body().getResponse().equals("exist")){
+//                setUIToWait(false);
+//
+//                Toast.makeText(SiginInActivity.this,"Same Data exists....",Toast.LENGTH_SHORT).show();
+//
+//            }
+//            else if(response.body().getResponse().equals("error")){
+//                setUIToWait(false);
+//
+//                Toast.makeText(SiginInActivity.this,"Something went wrong....",Toast.LENGTH_SHORT).show();
+//
+//            }
+//
+//            else{
+//                setUIToWait(false);
+//
+//                Toast.makeText(SiginInActivity.this,"Something went wrong....",Toast.LENGTH_SHORT).show();
+//
+//            }
+//            Intent intent = new Intent(SiginInActivity.this, SiginInActivity.class);
+//            startActivity(intent);
+//
+//        }
+//
+//        @Override
+//        public void onFailure(Call <Doument_Upload_Nil> call, Throwable t) {
+//            setUIToWait(false);
+//            Toast.makeText(SiginInActivity.this,"Update Failed",Toast.LENGTH_SHORT).show();
+//        }
+//    });
+//
+//
+//}
+
 
 
 
 }
+
